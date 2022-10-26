86c003a2834f168e751f43fe5e736822aba5f497
omurbekov-azamat <avgusttin8@gmail.com>
2022-10-26 16:48:20 +0600

    in component created useState with some tasks and id

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TaskForm from "./TaskForm/TaskForm";
 import Task from "./Task/Task"
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {task: 'buy milk', id: 'd123'},
+    {task: 'walk with dog', id: 'd442'},
+    {task: 'do homework', id: 'asd'},
+  ]);
+
+
+  const [showTasks, setShowTasks] = useState(false)
+
+  const toggleTasks = () => {
+      setShowTasks(!showTasks);
+  };
+
+  let tasksList: React.ReactNode = null;
+
+  if (showTasks) {
+    tasksList = (
+      <div>
+        <Task task={tasks[0].task}/>
+        <Task task={tasks[1].task}/>
+        <Task task={tasks[2].task}/>
+      </div>
+    );
+  }
+
   return (
     <>
-      <TaskForm/>
-      <Task task={'do your homework-53'}/>
+      <TaskForm onHeaderClick={(e) => {
+        e.preventDefault();
+        toggleTasks()
+      }}/>
+      {tasksList}
     </>
   );
 }
--- src/TaskForm/TaskForm.tsx
+++ src/TaskForm/TaskForm.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 
-const TaskForm = () => {
+interface TaskFormProps extends React.PropsWithChildren {
+  onHeaderClick: React.MouseEventHandler;
+}
+
+const TaskForm: React.FC<TaskFormProps> = (props) => {
   return (
     <div className='bg-light p-3 mb-3'>
       <form>
         <div className='container d-flex'>
           <input type="text" className='form-control' placeholder='Add new task'/>
-          <button className='btn btn-success ms-4'>Add</button>
+          <button onClick={props.onHeaderClick} type='submit' className='btn btn-success ms-4'>Add</button>
         </div>
       </form>
     </div>
